f7525062026d9fee0e9b65938516eff5d0f2925f
anzamarska <anzamarska@gmail.com>
2020-09-09 14:17:26 +0200

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3GvRJJ8UoC-5zTGPFUJVDKH4TXALilAQ",
+  authDomain: "e-challenge-99ec4.firebaseapp.com",
+  databaseURL: "https://e-challenge-99ec4.firebaseio.com",
+  projectId: "e-challenge-99ec4",
+  storageBucket: "e-challenge-99ec4.appspot.com",
+  messagingSenderId: "645239158223",
+  appId: "1:645239158223:web:5e8f297c0503ec22e70eba",
+  measurementId: "G-MGB6CJ0JGT",
+};

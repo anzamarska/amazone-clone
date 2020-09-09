1b2a77f9d2bfaa9c7f6489513983461d368c1e4d
anzamarska <anzamarska@gmail.com>
2020-09-09 17:51:40 +0200

    structured the header part and styled it

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+// rfce
+
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      <div className="header__search">
+        <input className="header__serachInput" type="text"></input>
+      </div>
+      <div class="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwi">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwi">Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwi">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;

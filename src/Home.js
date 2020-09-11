f6951004c655eb1ae65555a6fc984f02255fefa9
anzamarska <anzamarska@gmail.com>
2020-09-11 19:49:22 +0200

    change structure of home component

--- src/Home.js
+++ src/Home.js
@@ -13,17 +13,55 @@ function Home() {
         />
 
         <div className="home__row">
-          <Product />
-          {/* {Product} */}
+          <Product
+            id="1"
+            title="The lean startup"
+            price={29.22}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            rating={1}
+          />
+          <Product
+            id="2"
+            title="This big brown bear always greets you with open arms. Cuddly like no other and with a lovely giant stomach where you can feel safe resting - in other words, a real softy."
+            price={9999.99}
+            image="https://www.ikea.com/au/en/images/products/djungelskog-soft-toy__0710168_PE727370_S5.JPG"
+            rating={10}
+          />
         </div>
 
         <div className="home__row">
-          {/* {Product} */}
-          {/* {Product} */}
-          {/* {Product} */}
+          <Product
+            id="3"
+            title="If you love a cat with an exotic look but without the size and danger of a wild cat, the Bengal was developed with you in mind."
+            price="little more than you have got in your glass"
+            image="https://vetstreet.brightspotcdn.com/dims4/default/4f4dea1/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F31%2F1ba400a28511e087a80050568d634f%2Ffile%2FBengal-3-645mk062211.jpg"
+            rating={5}
+          />
+          <Product
+            id="4"
+            title="Pizza (Italian: [ˈpittsa], Neapolitan: [ˈpittsə]) is a savory dish of Italian origin consisting of salt in water"
+            price={29.22}
+            image="https://lh3.googleusercontent.com/proxy/1A1iu6Am4aa5naw2qrQuSA-vjfYWhhNATl5vXaO0SXacow2DsOyQGwpJttNQKnpZ1aFNqrLht2xrRMIJ5_FO7l_8C0FEmI5sAQfSVmaMq1ZtQ6d13Fwk"
+            rating={3}
+          />
+          <Product
+            id="5"
+            title="The lean startupBärenpark takes you into the world of bears, challenging you to build your own bear park. Would you like another polar bear enclosure or rather a koala* house?"
+            price={7.5}
+            image="https://cf.geekdo-images.com/itemrep/img/W_7m_ai2jJKmjnQr24qT6hY0Hzo=/fit-in/246x300/filters:strip_icc()/pic3486218.jpg"
+            rating={4}
+          />
         </div>
 
-        <div className="home__row">{/* {Product} */}</div>
+        <div className="home__row">
+          <Product
+            id="6"
+            title="Renault Clio R.S. Line 1.3 TCe 130KM EDC"
+            price={9399.0}
+            image="https://rozmiarfelgi.pl/img/models/renault-clio-2019-hatchback.jpg"
+            rating={8}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>the lean sturtup</p>
-      </div>
+        <p>{title}</p>
 
-      <p className="product__price">
-        <small>$</small>
-        <strong>12.34</strong>
-      </p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button>Add to busket</button>
     </div>
   );
 }

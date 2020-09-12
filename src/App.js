cd198109062e52afbf7b6cd5e0ff90e9cf6cb0c6
anzamarska <anzamarska@gmail.com>
2020-09-12 22:16:03 +0200

    build Subtotal component, styled checkout page, add links to property part of site

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            {/* thats mean "when im on that path show me this components:" */}
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            {/* thats mean "when im on that path show me this components:" */}
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
       <div className="header__search">
         <input className="header__serachInput" type="text"></input>
         <SearchIcon className="header__serachIcon" />
@@ -29,11 +32,13 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
-        </div>
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+        </div>{" "}
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );

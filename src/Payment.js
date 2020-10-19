1dc8365be2d7007823a5bf879a373c2f6f267703
unknown <anzamarska@gmail.com>
2020-10-19 16:15:43 +0200

    styled payment part

--- src/Payment.js
+++ src/Payment.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CheckoutProduct from './CheckoutProduct';
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
+import { Link } from "react-router-dom";
 
 
 function Payment() {
-    const [{user}, dispatch] = useStateValue();
+    const [{user, basket}, dispatch] = useStateValue();
 
 
     return (
@@ -16,13 +18,34 @@ function Payment() {
                     </div>
                     <div className="payment__address">
                         <p>{user?.email}</p>
+                        <p>Niedzwiedzia 12542</p>
+                        <p>WWA, Poland</p>
                     </div>
                 </div>
                 <div className='payment__section'>
-
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className='payment__items'>
+                        {basket.map(item => 
+                            <CheckoutProduct
+                                id={item.id}
+                                image={item.image}
+                                title={item.title}
+                                price={item.price}
+                                rating={item.rating}
+                            />)}
+                    </div>
 
                 </div>
                 <div className='payment__section'>
+                    <div className="payment__title">
+                        <h3>Payment Mathod</h3>
+
+                    </div>
+                    <div className='payment__details'>
+
+                    </div>
 
 
                 </div>
